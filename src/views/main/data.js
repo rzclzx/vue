e4b8e414dc69922a1a33cae3c3494ed72934a8ca
zhangfei <1520175722@qq.com>
2024-12-14 07:12:42 +0800

    feat：vue3init

---
+++ src/views/main/data.js
@@ -0,0 +1,42 @@
+import _ from 'lodash'
+const getData = () => {
+  return _.cloneDeep([
+    {
+      ind: 0,
+      a: 0,
+      b: 0,
+      c: 0
+    },
+    {
+      ind: 1,
+      a: 0,
+      b: 0,
+      c: 0
+    },
+    {
+      ind: 2,
+      a: 0,
+      b: 0,
+      c: 0
+    },
+    {
+      ind: 3,
+      a: 0,
+      b: 0,
+      c: 0
+    },
+    {
+      ind: 4,
+      a: 0,
+      b: 0,
+      c: 0
+    },
+    {
+      ind: 5,
+      a: 0,
+      b: 0,
+      c: 0
+    }
+  ])
+}
+export default getData
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,183 +1,158 @@
 <template>
   <div class="app-container">
-    <vxe-table
-      border
-      resizable
-      height="600"
-      ref="xTree"
-      :tree-config="{}"
+    <div class="flex-start-center">
+      <span style="width: 100px">总资金：</span>
+      <el-input-number :controls="false" v-model="total" style="width: 180px" size="mini"></el-input-number>
+    </div>
+    <el-divider></el-divider>
+    <div class="flex-start-center">
+      <span style="width: 100px">当前档位：</span>
+      <el-input-number :controls="false" v-model="current" style="width: 180px" size="mini"></el-input-number>
+    </div>
+    <el-divider></el-divider>
+    <div class="flex-start-center">
+      <span style="width: 100px">止盈点：</span>
+      <el-input-number :controls="false" v-model="mylimit" style="width: 180px" size="mini"></el-input-number>
+    </div>
+    <el-divider></el-divider>
+    <div class="flex-start-center">
+      <span style="width: 100px">止损点：</span>
+      <span>{{ limit }}</span>
+    </div>
+    <el-divider></el-divider>
+    <div class="flex-start-center">
+      <span style="width: 100px">止损总亏：</span>
+      <span>{{ limitm }}</span>
+    </div>
+    <el-divider></el-divider>
+    <div class="flex-start-center">
+      <el-button type="primary" size="mini" @click="todo">固定价止盈</el-button>
+      <i class="el-icon-refresh link" style="margin-left: 50px; font-size: 30px" @click="refresh"></i>
+    </div>
+    <el-divider></el-divider>
+    <el-table
       :data="data"
-    >
-      <vxe-column
-        v-for="(item, index) in columns"
-        :key="item.value"
-        :field="item.value"
-        :title="item.label"
-        :width="index === 0 ? 400 : ''"
-        :tree-node="index === 0 ? true : false"
-      ></vxe-column>
-    </vxe-table>
+      border
+      size="mini"
+      highlight-current-row
+      style="width: 100%">
+      <el-table-column
+        width="50"
+        prop="ind"
+        label="序号">
+      </el-table-column>
+      <el-table-column
+        prop="a"
+        label="买入点">
+        <template slot-scope="scope">
+          <el-input-number :controls="false" size="mini" v-if="scope.$index == 0" :value="scope.row.a" @change="change"></el-input-number>
+          <span v-else>{{ scope.row.a }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="b"
+        label="止盈点">
+      </el-table-column>
+      <el-table-column
+        prop="c"
+        label="买入金额">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button type="text" @click="ok(scope)">止盈</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div>总结余：{{ value }}</div>
+    <div>总收益：{{ valuelen }}</div>
   </div>
 </template>
-
 <script>
-import data from './bigData'
+import getData from './data'
 export default {
   data() {
     return {
-      columns: [
-        {
-          value: "name",
-          label: "变量名",
-          minWidth: 240,
-          align:'left',
-          edit: 'input',
-          filterable: true,
-          filterReplaceKey: 'value',
-          remark: '变量ApplicationInfo，不可修改，删除，剪切',
-          rules: [
-            { required: true, message: '请输入变量名' },
-            { pattern: /^([a-z]|[A-Z])/, message: '首位为字母' },
-          ]
-        },
-        {
-          value: "describe",
-          label: "变量中文名",
-          minWidth: 180,
-          align:'left',
-          filterable: true,
-          edit: 'input',
-          rules: [
-            { required: true, message: '请输入变量名' },
-            { max: 50, message: '不能超过50个字符' }
-          ]
-        },
-        {
-          value: "type",
-          label: "数据类型",
-          edit: 'select',
-          align:'center',
-          default: 'string',
-          selectList: [
-            {
-              label: "string",
-              value: "string",
-            },
-            {
-              label: "int",
-              value: "int",
-            },
-            {
-              label: "double",
-              value: "double",
-            },
-            {
-              label: "date",
-              value: "date",
-            },
-            {
-              label: "datetime",
-              value: "datetime",
-            },
-            {
-              label: "boolean",
-              value: "boolean",
-            },
-            {
-              label: "object",
-              value: "object",
-            },
-          ]
-        },
-        {
-          value: "isArr",
-          label: "是否数组",
-          edit: 'select',
-          default: '0',
-          disClearable: true,
-          selectList: [
-            {
-              label: '是',
-              value: '1'
-            },
-            {
-              label: '否',
-              value: '0'
-            },
-          ],
-          trans: value => {
-            if (value === '否') {
-              return ''
-            } else {
-              return value
-            }
-          }
-        },
-        {
-          value: "isEmpty",
-          label: "是否可为空",
-          edit: 'select',
-          default: '0',
-          disClearable: true,
-          selectList: [
-            {
-              label: '是',
-              value: '1'
-            },
-            {
-              label: '否',
-              value: '0'
-            },
-          ],
-          trans: value => {
-            if (value === '否') {
-              return ''
-            } else {
-              return value
-            }
-          }
-        },
-        {
-          value: "enumName",
-          label: "数据字典",
-          edit: 'select',
-          selectList: [],
-          link: 'el-icon-share'
-        },
-        {
-          value: "isUse",
-          label: "是否使用",
-          default: '0',
-          selectList: [
-            {
-              label: '是',
-              value: '1'
-            },
-            {
-              label: '否',
-              value: '0'
-            },
-          ],
-          trans: value => {
-            if (value === '否') {
-              return ''
-            } else {
-              return value
-            }
-          },
-          link: 'el-icon-share',
-          linkAlowEqualsValue: '1',
-          remark: '变量被组件使用则表示已使用，已使用的变量不允许修改、剪切、删除'
-        },
-      ],
-      data
+      // 当前档位
+      current: undefined,
+      // 总资金
+      total: undefined,
+      // 止损点
+      limit: 0,
+      // 止损总亏
+      limitm: 0,
+      // 止盈点
+      mylimit: undefined,
+      // 总结余
+      value: 0,
+      // 止盈倍数
+      scale: 1.5,
+      // 总收益
+      valuelen: 0,
+      // 数据
+      data: getData()
     }
   },
   methods: {
-    
+    /**
+     * 重置所有数据
+     */
+    refresh() {
+      this.current = undefined
+      this.total = undefined
+      this.limit = 0
+      this.limitm = 0
+      this.mylimit = undefined
+      this.value = 0
+      this.valuelen = 0
+      this.data = getData()
+    },
+    trans(value) {
+      let a = value * 100000000
+      let b = Math.round(a)
+      return b / 100000000
+    },
+    change(val) {
+      if (!this.total) {
+        alert('请输入总金额')
+        return
+      }
+      this.data[0].a = val
+      this.data[0].b = this.trans(this.data[0].a * this.scale)
+      this.data[0].c = this.total * 0.5
+      for (let i = 1; i < this.data.length; i++) {
+        this.data[i].a = this.trans(this.data[0].a * Math.pow(0.9, i))
+        this.data[i].b = this.trans(this.data[i].a * (this.scale + 0.1*i))
+        this.data[i].c = this.total * 0.1
+      }
+      this.limit = this.trans(this.data[0].a * Math.pow(0.9, 6))
+      this.limitm = this.trans(this.total * 0.3657205)
+    },
+    todo() {
+      if (this.current === undefined || this.mylimit === undefined) {
+        alert('请输入当前档位和止盈点')
+        return
+      }
+      this.ok({}, this.current, this.mylimit)
+    },
+    ok(scope, index, mylimit) {
+      if (!this.total || !this.data[0].a) {
+        alert('请输入总资金和买入点')
+        return
+      }
+      this.current = index === undefined ? scope.$index : index
+      this.mylimit = mylimit === undefined ? scope.row.b : mylimit
+      this.valuelen = 0
+      let value = this.mylimit
+      for (let i = 0; i < this.data.length; i++) {
+        let m = i === 0 ? this.total * 0.5 : this.total * 0.1
+        this.valuelen += this.trans(((value - this.data[i].a) / this.data[i].a) * m)
+        if (this.current == i) {
+          this.value = this.total + this.valuelen
+          return
+        }
+      }
+    }
   }
 }
 </script>
-
-<style lang="less" scoped>
-</style>
